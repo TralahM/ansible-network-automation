095e2faca2a8a668b8ddd6d931d16014049b2fc9
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2020-01-09 02:28:28 +0300

    upd'

--- init_ans_proj.sh
+++ init_ans_proj.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env sh
 echo "Initializing Ansible Project ..."
-find . -type d -exec ad \{ production, staging \}/hosts \;
+ad production/hosts
+ad production/hosts
+ad group_vars/{all,local,hosts}
+ad host_vars/{all,local,hosts}
 
-find . -type d -exec ad \{group_vars/{ all, local \} \;
-find . -type d -exec ad \{host_vars/{ docker, local \} \;
+touch {site.yml,webservers.yml,dbservers.yml,networkservers.yml}
+mkdir -p roles/{common,webtier,monitoring,database,nginx}/{tasks,handlers,vars,defaults,meta}
 
-find . -type d -exec ad  \{ site.yml, webservers.yml, dbservers.yml, networkservers.yml \} \;
-
-find . -type d -exec ad \
-\roles/\{ common, webtier, monitoring, db \}/{ tasks, handlers, vars, defaults, meta \}/main.yml \;
-find . -type d -exec ad \{roles/{ common,webtier,monitoring,db \}/templates/{ nginx.conf.j2,apache.conf.j2 \} \;
-find . -type d -exec ad \{roles/common/files/{ .vimrc, .zshrc, .tmux.conf, .tmux.conf.local \} \;
-find . -type d -exec cp ~/\{ .vimrc, .zshrc, .tmux.conf, .tmux.conf.local \} ./roles/common/files \;
-
-
-echo "Initializing Playbook templates....."
-
-echo "---\n# file: site.yml [master-playbook]\n- import_playbook: webservers.yml\n- import_playbook: dbservers.yml\n- import_playbook: networkservers.yml" > site.yml
-
-echo "---\n# file: webservers.yml\n- hosts: webservers\n\troles:\n\t\t- common\n\n\t- webtier" > webservers.yml
-
-echo "---\n# file: dbservers.yml\n- hosts: dbservers\n\troles:\n\t\t- common\n\n\t- db" > dbservers.yml
-
-echo "---\n# file: networkservers.yml\n- hosts: networkservers\n\troles:\n\t\t- common\n\n\t- monitoring" > networkservers.yml
+ad roles/{common,webtier,monitoring,db}/{tasks,files,templates,handlers,vars,defaults,meta}/main.yml
 
 echo "Done"
-# tree
